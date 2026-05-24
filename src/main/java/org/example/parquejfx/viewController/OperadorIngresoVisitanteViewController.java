02b45b2a605fff7597cd53a82e4d47e39f2ba8ff
Santiago15110 <davids.mamianz@uqvirtual.edu.co>
2026-05-24 02:24:31 -0500

    Se agrÂegó una pagina de scene builder y se conecto con el archivo viewController

---
+++ src/main/java/org/example/parquejfx/viewController/OperadorIngresoVisitanteViewController.java
@@ -0,0 +1,29 @@
+package org.example.parquejfx.viewController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class OperadorIngresoVisitanteViewController {
+
+        @FXML
+        private Button btnValidar;
+
+        @FXML
+        private Button btnVolver;
+
+        @FXML
+        private TextField txtDocumento;
+
+        @FXML
+        void validar(ActionEvent event) {
+
+        }
+
+        @FXML
+        void volver(ActionEvent event) {
+
+        }
+
+}
